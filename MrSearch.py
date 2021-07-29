19c28f0a2fa2fe3091b8425fc4e5547a9b3c2aff
MRX8001 <guiluiscom08@gmail.com>
2021-07-29 15:15:35 -0300

    MrSearch atualizado

--- MrSearch.py
+++ MrSearch.py
@@ -5,6 +5,7 @@ except ModuleNotFoundError:
 	os.system('python3 -m pip install -r requirements.txt')
 os.system('pyfiglet -L tools/3D-ASCII.flf; clear')
 pyfiglet.print_figlet('MRX800', '3D-ASCII')
+print('\033[1;37mATIVE O TOR ANTES DE FAZER OS TESTES COM O MrSearch\033[m\n')
 uagents = random.choice(open('tools/user-agents.txt', 'r', encoding='UTF-8', errors='ignore').readlines())
 class MrSearch:
 	def __init__(self, query, num, user_agent=uagents):
@@ -27,21 +28,26 @@ class MrSearch:
 				print('\033[1;34mSearching for vulns........')
 				blind = blinder.blinder(result, sleep=5, ua=self.user_agent, proxy={'http': 'socks5://127.0.0.1:9050', 'https': 'socks5://127.0.0.1:9050'})
 				scan = blind.check_injection()
-				sqlscan = sql_scan.fuzz(url=result, user_agent=self.user_agent, proxy={'http': 'socks5://127.0.0.1:9050', 'https': 'socks5://127.0.0.1:9050'})
-				scan_page = dsss.scan_page(url=result)
-				if scan == True or scan_page == True or sqlscan == True:
-					if scan == True:
-						print('\033[1;4;33mO alvo possui a vuln Time-based Blind SQLI\033[m')
-					elif scan_page == True:
-						print('\033[1;4;33mO alvo está vulnerável a ataques de SQLI\033[m')
+				if scan == True:
+					print('\n\033[1;4;33mO alvo possui a vuln Time-based Blind SQLI\033[m\n')
 				else:
 					pass
+					scan_page = dsss.scan_page(url=result)
+					if scan_page == True:
+						print('\n\033[1;4;33mO alvo está vulnerável a ataques de SQLI\033[m\n')
+					else:
+						pass
+						sqlscan = sql_scan.fuzz(url=result, user_agent=self.user_agent, proxy={'http': 'socks5://127.0.0.1:9050', 'https': 'socks5://127.0.0.1:9050'})
+						if sqlscan == True:
+							print('\n\033[1;4;33mO alvo possui a vuln Time-based Blind SQLI\033[m\n')
+						else:
+							print('\n\033[1;4;31mNão consegui detectar alguma vuln de SQLI\033[m\n')
 			else:
 				pass
 if __name__ == '__main__':
 	try:
 		parser = argparse.ArgumentParser(description='Protótipo para fazer buscas com o Google')
-		parser.add_argument('--search', type=str, help='Setar o parâmetro de busca', required=False)
+		parser.add_argument('--search', type=str, help='Parâmetro de busca', required=True)
 		parser.add_argument('--num', type=int, help='Definir a quantidade de links para buscar', required=True)
 		parser.add_argument('--user-agent', type=str, help='Definir um User-Agent (Recomendado) (Padrão: Randomico)', required=False)
 		args = parser.parse_args()
